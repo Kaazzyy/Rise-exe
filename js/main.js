fd96b3c0423b426490a1a437a9161e4e8b35ae09
Kaazzyy <maneljoao383@gmail.com>
2024-06-24 23:52:55 +0100

    main.js

--- js/main.js
+++ js/main.js
@@ -20,6 +20,12 @@
             isLockedToName:false,
             url:'https://i.postimg.cc/x82447k4/hat5.png',
             forceSkin:'https://i.postimg.cc/QxjCrd1f/skin5.png'
+
+            name:'sugner',
+            skinUrl:'https://skins.vanis.io/s/LC1msC',
+            isLockedToColor:false,
+            isLockedToName:false,
+            url:'https://i.postimg.cc/x82447k4/hat5.png',
         }
     ]
     
