965eb8d9beab7030293fc4b103395f267f17a8d3
Kaazzyy <maneljoao383@gmail.com>
2024-07-19 02:15:21 +0100

    main.js

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@
     
     CellOverlayManager.list = [
         {
-            name:'sugner',
-            skinUrl:'https://skins.vanis.io/s/LC1msC',
+            name:'',
+            skinUrl:'',
             isLockedToColor:false,
             isLockedToName:false,
-            url:'https://i.postimg.cc/x82447k4/hat5.png',
-            forceSkin:'https://skins.vanis.io/s/LC1msC'
+            url:'',
+            forceSkin:''
         }
     ]
     
